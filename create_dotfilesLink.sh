052f2676a9f6f9a4a2772ecf5c06de30f6af8007
tatsuki nakagawa <nyoronyoro-kun@tatsuki-no-MacBook-Pro.local>
2016-06-27 01:12:09 +0900

    gitconfig, bash_profile, bash_rcのファイル存在確認およびシンボリックリンク作成コマンドを追加

--- create_dotfilesLink.sh
+++ create_dotfilesLink.sh
@@ -27,3 +27,37 @@ if [ ! -e ~/.vim/bundle ]; then
 else
     echo '既にディレクトリ ~/.vim/bundleは存在します'
 fi
+
+# Git設定ファイルの存在確認
+if [ ! -e ~/.gitconfig ]; then
+	ln -Fis ~/dotfiles/.gitconfig ~/.gitconfig
+else
+	echo '~/.gitconfigは存在します'
+fi
+
+# bash_rcの設定ファイルの存在確認
+if [ ! -e ~/.bash_rc ]; then
+    ln -Fis ~/dotfiles/.bash_rc ~/.bash_rc
+    echo "bash_rcがホームディレクトリに無かったので作成しました";
+else
+    echo '~/.bash_rcは存在します'
+fi
+
+# bash_profileの設定ファイル存在確認
+if [ -f $HOME/.bash_profile ]; then
+    # bash_profileにbash_rcの読み込み設定が記載されているかどうか
+    IS_BASH_RC=$(cat $HOME/.bash_profile | grep "bash_rc\|bashrc");
+    if [ -n "$IS_BASH_RC" ]; then
+        echo "既にbash_profileにbash_rcの読み込みが記載されています";
+    else
+        echo ". $HOME/.bash_rc" >> "$HOME/.bash_profile";
+        echo "bash_profileにbash_rc読み込みを追記しました";
+        source $HOME/.bash_profile
+    fi
+else
+    touch $HOME/.bash_profile
+    echo "bash_profileがホームディレクトリに無かったので作成しました";
+    echo ". $HOME/.bash_rc" >> "$HOME/.bash_profile";
+    echo "bash_profileにbash_rc読み込みを追記しました";
+    source $HOME/.bash_profile
+fi
